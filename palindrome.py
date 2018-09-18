60bb77a5a0fc09cf60432d6d8871704b2b61bcb4
MEENAKAMALI <41040130+MEENAKAMALI@users.noreply.github.com>
2018-09-18 16:41:18 +0530

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -4,8 +4,8 @@ rev=0
 while(n>0):
     dig=n%10
     rev=rev*10+dig
-    n=n//10
+    n=n/10
 if(temp==rev):
-    print("The number is a palindrome!")
+    print("palindrome!")
 else:
-    print("The number isn't a palindrome!")
+    print("not a palindrome!")
