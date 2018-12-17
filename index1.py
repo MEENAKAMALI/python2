5fb1235e40afd396928062806ee806532d17788c
MEENAKAMALI <41040130+MEENAKAMALI@users.noreply.github.com>
2018-12-17 10:30:19 +0530

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,8 @@
+from array import*
+arr=array('i',[])
+n=int(input("enter the length"))
+for i in range(3):
+   	 x= int(input("enter the next value"))
+    	arr.append(x)
+	print(arr)
+	print(arr.index(x))
